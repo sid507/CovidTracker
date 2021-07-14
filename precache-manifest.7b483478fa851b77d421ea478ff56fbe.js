523690b46a6aeb356d676c34305533972ae9d2ec
sid507 <ssm2@somaiya.edu>
2021-07-14 10:57:28 +0530

    Updates

---
+++ precache-manifest.7b483478fa851b77d421ea478ff56fbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90ffe12e0160bf6a9d6857c10e6be49",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "ece59a29061de4c846db",
+    "url": "/CovidTracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d37071b5e36dff6a3c29",
+    "url": "/CovidTracker/static/css/main.6177b4cd.chunk.css"
+  },
+  {
+    "revision": "ece59a29061de4c846db",
+    "url": "/CovidTracker/static/js/2.3ee9e8bf.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/CovidTracker/static/js/2.3ee9e8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37071b5e36dff6a3c29",
+    "url": "/CovidTracker/static/js/main.e835dfeb.chunk.js"
+  },
+  {
+    "revision": "dd0e644dc5b5b996215d",
+    "url": "/CovidTracker/static/js/runtime-main.dfc585e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CovidTracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
